94fcadcdc97800f847f05b41c1772e87bd48fbef
Afreenpinjari <afreenpinjari@gmail.com>
2025-10-10 11:31:40 +0530

    Fix SearchBox import case for Render build

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -1,6 +1,6 @@
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
-import "./search.css";
+import "./Search.css";
 import { useState } from "react";
 
 export default function SearchBox({ updateInfo }) {
